705a56295ba48fcb6dee8ec345db3c370308c524
Paulo Elienay <paulo@pauloelienay.com>
2021-09-02 11:53:20 -0300

    feat: implements `ProfissionalRepository`

---
+++ src/main/java/com/pauloelienay/mv/repository/ProfissionalRepository.java
@@ -0,0 +1,7 @@
+package com.pauloelienay.mv.repository;
+
+import com.pauloelienay.mv.domain.Profissional;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ProfissionalRepository extends PagingAndSortingRepository<Profissional, Long> {
+}
