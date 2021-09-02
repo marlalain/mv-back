9c58fc728c37b895570bb04a510d33663384116c
Paulo Elienay <paulo@pauloelienay.com>
2021-09-02 11:34:21 -0300

    chore: inits project

---
+++ src/test/java/com/pauloelienay/mv/MvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloelienay.mv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
